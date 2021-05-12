1a88837c72b98aa57e44e4cfef3753565a6c4528
Subrahmanyam Yadav <28subrahmanyam@gmail.com>
2021-05-12 12:18:21 +0530

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/json-to-XML-0.0.1-SNAPSHOT.jar
--- src/main/java/com/subbu/json_to_XML/App.java
+++ src/main/java/com/subbu/json_to_XML/App.java
@@ -1,13 +1,24 @@
 package com.subbu.json_to_XML;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Hello world!
  *
  */
+@Controller
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
+	
+	@RequestMapping("/")
+	String index() {
+		return "index";
+	}
 }
